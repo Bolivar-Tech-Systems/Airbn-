681eca37f7df6be9f5979d2dc49ba8b00544bbb4
Ian Novoa <iannovoacc8@gmail.com>
2026-03-02 12:24:08 -0500

    Correccion errores en main y test

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'horario.dart';
 
 void main() {
-  runApp(MiApp());
+  runApp(MyApp());
 }
 
-class MiApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false, home: Horario());
